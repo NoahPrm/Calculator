c4bfe2fe9d910a5f888b1536628e9feab5848b89
Noah <108767214+NoahPrm@users.noreply.github.com>
2023-12-16 17:55:19 +0100

    🔰 Updated Windows executable files

--- Windows/calculator.mjs
+++ Windows/calculator.mjs
@@ -1,26 +1,35 @@
-import readline from 'readline';
-import chalk from 'chalk';
-import figlet from 'figlet';
+import readline from "readline";
+import chalk from "chalk";
+import figlet from "figlet";
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 function main() {
   console.clear();
-  const banner = figlet.text('Calculator', (error, asciiText) => {
-    console.log(chalk.blue.bold(asciiText))
-    rl.question(chalk.blue.bold('Enter an expression to calculate\n> '), (input) => {
-        try {
-          const result = eval(input);
-          console.log(chalk.green(`Result : ${result}`));
-        } catch (error) {
-          console.error(chalk.red.bold('Error :'), error.message);
-        }
-        rl.close();
-      });
-  })
+  const banner = figlet.text("Calculator", (error, asciiText) => {
+    console.log(chalk.blue.bold(asciiText));
+    calculate();
+  });
+}
+
+function calculate() {
+  rl.question(
+    chalk.blue.bold("Enter an expression to calculate\n> "),
+    (input) => {
+      try {
+        const result = eval(input);
+        console.log(chalk.green(`Result : ${result}`));
+      } catch (error) {
+        console.error(chalk.red.bold("Error :"), error.message);
+      }
+
+      console.log(chalk.blue.bold("\nReady for the next calculation...\n"));
+      calculate();
+    }
+  );
 }
 
 main();
